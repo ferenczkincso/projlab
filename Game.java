fd448ac9a1b49eec9dfe3be81bfc242a05c67420
Csibi Alexandra <alexandracsibi@yahoo.com>
2022-03-26 23:01:58 +0100

    5.3.13

--- Game.java
+++ Game.java
@@ -3,14 +3,21 @@ import java.util.List;
 public class Game {
 
     private List<Virologist> virologists;
+    private List<Field> fields;
 
     public void EndGame(){}
 
-    public void CreateCity(){}
+    public void CreateCity(){
+        System.out.println("CreateCity()");
+    }
 
-    public void AddVirologist(){}
+    public void AddVirologist(){
+        System.out.println("AddVirologist()");
+    }
 
     public void NewGame(){
         System.out.println("NewGame()");
+        AddVirologist();
+        CreateCity();
     }
 }
