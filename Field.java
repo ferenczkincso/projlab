30dccb0dbdc2c0762ae69560c3fe0dff8c5a0c70
Csibi Alexandra <alexandracsibi@yahoo.com>
2022-03-26 22:38:59 +0100

    5.3.1

--- Field.java
+++ Field.java
@@ -3,12 +3,15 @@ import java.util.List;
 public class Field {
     protected int fieldID;
     private List<Field> neighbours;
+    private Virologist virologist;
 
     public List<Field> GetNeighbours(){
         System.out.println("GetNeighbours");
         return neighbours;
     }
-    public Virologist GetVirologistNearby(){}
+    public Virologist GetVirologistNearby(){
+        return virologist;
+    }
     public void Accept(Virologist v){
         System.out.println("Accept");
     }
