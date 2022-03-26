7883d5c5968e293d5206b75765b61ebe955ec533
Csibi Alexandra <alexandracsibi@yahoo.com>
2022-03-26 22:36:42 +0100

    5.3.1

--- Field.java
+++ Field.java
@@ -4,10 +4,17 @@ public class Field {
     protected int fieldID;
     private List<Field> neighbours;
 
-    public List<Field> GetNeighbours(){}
+    public List<Field> GetNeighbours(){
+        System.out.println("GetNeighbours");
+        return neighbours;
+    }
     public Virologist GetVirologistNearby(){}
-    public void Accept(Virologist v){}
-    public void Remove(Virologist v){}
+    public void Accept(Virologist v){
+        System.out.println("Accept");
+    }
+    public void Remove(Virologist v){
+        System.out.println("Remove");
+    }
     public void Collect(Virologist v){}
 
 }
