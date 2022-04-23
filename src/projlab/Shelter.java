09d1ed2603b996228e3b7ca2ac5b16ce865ffe20
Csibi Alexandra <alexandracsibi@yahoo.com>
2022-04-23 21:56:27 +0200

    shelter changes

--- src/projlab/Shelter.java
+++ src/projlab/Shelter.java
@@ -1,10 +1,21 @@
 package projlab;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Shelter extends Field{
     Protection protection;
 
+    public Shelter(){
+        Random rand = new Random();
+        int id = rand.nextInt(4);
+        switch (id){
+            case 0: protection = new Bag();
+            case 1: protection = new Cloak();
+            case 2: protection = new Glove();
+            case 3: protection  = new Ax();
+        }
+    }
     /**
      * A menedékben tartózkodó virológus begyűjti a
      * védőfelszerelést
