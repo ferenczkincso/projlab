1fc2d8575764845d5bed32098e986564144b8c96
Csibi Alexandra <alexandracsibi@yahoo.com>
2022-04-28 16:21:00 +0200

    Display

---
+++ src/projlab/InventoryDisplay.java
@@ -0,0 +1,11 @@
+package projlab;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class InventoryDisplay implements Observer {
+    @Override
+    public void update(Observable o, Object arg) {
+
+    }
+}
