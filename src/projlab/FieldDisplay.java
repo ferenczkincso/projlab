bd615ee87ffe10a54e303c88fae018766388bdf3
Csibi Alexandra <alexandracsibi@yahoo.com>
2022-05-10 12:13:53 +0200

    kicsi kicsi valtoztatasok

--- src/projlab/FieldDisplay.java
+++ src/projlab/FieldDisplay.java
@@ -3,9 +3,12 @@ package projlab;
 import java.util.Observable;
 import java.util.Observer;
 
-public class FieldDisplay implements Observer {
+public class FieldDisplay implements Observer{
+    Field f;
     @Override
     public void update(Observable o, Object arg) {
+        if (o.getClass() == f.getClass()){
 
+        }
     }
 }
--- src/projlab/VirologistDisplay.java
+++ src/projlab/VirologistDisplay.java
@@ -4,9 +4,11 @@ import java.util.Observable;
 import java.util.Observer;
 
 public class VirologistDisplay implements Observer {
-
+    Virologist v;
     @Override
     public void update(Observable o, Object arg) {
+        if (o.getClass() == v.getClass()){
 
+        }
     }
 }
