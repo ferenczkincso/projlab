1df531863176a301474261d7c866e380cd58c2fd
Csibi Alexandra <alexandracsibi@yahoo.com>
2022-05-13 20:08:12 +0200

    asd

--- src/projlab/VirologistDisplay.java
+++ src/projlab/VirologistDisplay.java
@@ -4,11 +4,8 @@ import java.util.Observable;
 import java.util.Observer;
 
 public class VirologistDisplay implements Observer {
-    Virologist v;
     @Override
     public void update(Observable o, Object arg) {
-        if (o.getClass() == v.getClass()){
 
-        }
     }
 }
